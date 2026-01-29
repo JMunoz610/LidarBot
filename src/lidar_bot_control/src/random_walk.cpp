954b21036415b81ece3bc678a575a9ab89240c2e
JMunoz610 <jmunoz3601@gmail.com>
2026-01-29 13:29:56 -0500

    added odometry callback

--- src/lidar_bot_control/src/random_walk.cpp
+++ src/lidar_bot_control/src/random_walk.cpp
@@ -1,43 +1,81 @@
 #include <chrono>
 #include <memory>
 #include <string>
+#include <cmath>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+#include <tf2/LinearMath/Matrix3x3.h>
 
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses a fancy C++11 lambda
-* function to shorten the callback syntax, at the expense of making the
-* code somewhat more difficult to understand at first glance. */
-
-class MinimalPublisher : public rclcpp::Node
+class RandomWalk : public rclcpp::Node
 {
 public:
-  MinimalPublisher()
-  : Node("minimal_publisher"), count_(0)
+  RandomWalk()
+  : Node("random_walk"), count_(0)
   {
-    publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
-    auto timer_callback =
+    // publisher callback
+    auto vel_callback =
       [this]() -> void {
-        auto message = std_msgs::msg::String();
-        message.data = "Hello, world! " + std::to_string(this->count_++);
-        RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
-        this->publisher_->publish(message);
+        
+        geometry_msgs::msg::Twist msg;
+        msg.linear.x = 0.5;
+        msg.angular.z = 0.0;
+
+        RCLCPP_INFO(this->get_logger(), "Publishing cmd_vel");
+        this->publisher_->publish(msg);
       };
-    timer_ = this->create_wall_timer(500ms, timer_callback);
+
+    // subscriber callback
+    auto pose_callback =
+      [this](nav_msgs::msg::Odometry::UniquePtr msg) -> void {
+        const auto &position = msg->pose.pose.position;
+        // const auto &linear = msg->twist.twist.linear;
+        // const auto &angular = msg->twist.twist.angular;
+        const auto &orientation = msg->pose.pose.orientation;
+
+        tf2::Quaternion q(orientation.x, orientation.y, orientation.z, orientation.w);
+
+        double roll, pitch, yaw;
+        tf2::Matrix3x3(q).getRPY(roll, pitch, yaw);
+
+        // send to global variable
+        pose_.x = position.x;
+        pose_.y = position.y;
+        pose_.heading = yaw;
+        RCLCPP_INFO(this->get_logger(), "Pose: x=%.2f y=%.2f heading=%.2f", pose_.x, pose_.y, pose_.heading*180.0/M_PI);
+      };
+
+    // publisher
+    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+    vel_timer_ = this->create_wall_timer(500ms, vel_callback);
+
+    // subscriber
+    subscriber_ = this->create_subscription<nav_msgs::msg::Odometry>("odom", 10, pose_callback);
   }
 
 private:
-  rclcpp::TimerBase::SharedPtr timer_;
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  struct Pose2D{
+    double x = 0.0;
+    double y = 0.0;
+    double heading = 0.0;
+  };
+  Pose2D pose_;
+  rclcpp::TimerBase::SharedPtr vel_timer_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscriber_;
   size_t count_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<RandomWalk>());
   rclcpp::shutdown();
   return 0;
 }
